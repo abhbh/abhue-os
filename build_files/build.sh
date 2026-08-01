27ebb653ede400128dfc40b4c5ba1e4d8a7ee5ba
Abhinav Kumar <abhnv@abhnv.com>
2026-08-01 17:37:57 +0000

    Initial setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,11 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+# Install the shell and compositor I use
+dnf5 install -y fish niri noctalia
+
+# Install other important packages
+# dnf5 install -y
 
 # Use a COPR Example:
 #
